f6702ca61e683f4b34063d2f5f3a87a25fa6bc58
SAC-CS112-Ngo-Son <frank.ngo.1702@gmail.com>
2015-11-18 19:41:44 -0800

    lab 1

---
+++ src/Dice.java
@@ -0,0 +1,26 @@
+//Son Ngo
+
+import java.util.Random;
+
+public class Dice
+{
+	private int iDices, iBounces, iThrow, iValue = 0;
+	
+	public int Throw (int dices)
+	{
+		Random rand = new Random();
+		return iThrow = rand.nextInt(6) + 1;
+	}
+	
+	public int Throw (int dices, int bounces)
+	{
+		Random rand = new Random();
+		for (int i = 1; i<= bounces; i++)
+		{
+			iValue = iValue + rand.nextInt(6) + 1;
+		}
+		iValue = iValue / bounces;
+		
+		return iThrow = dices * iValue;
+	}
+}
---
+++ src/Week12_Lab1.java
@@ -0,0 +1,22 @@
+
+//Son Ngo
+
+import javax.swing.JOptionPane;
+
+public class Week12_Lab1
+{
+	public static void main(String[]args)
+
+	{
+		int iDices, iBounces, iRoll;
+
+		iDices = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of dices: "));
+		iBounces = Integer.parseInt(JOptionPane.showInputDialog("Enter the number of bounces: "));
+		Dice dice = new Dice();
+		if (iDices == 1)
+			iRoll = dice.Throw(iDices);
+		else
+			iRoll = dice.Throw(iDices, iBounces);
+		JOptionPane.showMessageDialog(null, iRoll);
+	}
+}
