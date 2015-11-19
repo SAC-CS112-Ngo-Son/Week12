d3f0fb2bca2c9ee346d8c2e0a58bb3b994648d4b
SAC-CS112-Ngo-Son <frank.ngo.1702@gmail.com>
2015-11-18 20:08:59 -0800

    lab 2

--- src/Dice.java
+++ src/Dice.java
@@ -6,21 +6,34 @@ public class Dice
 {
 	private int iDices, iBounces, iThrow, iValue = 0;
 	
-	public int Throw (int dices)
+	public Dice()
 	{
-		Random rand = new Random();
-		return iThrow = rand.nextInt(6) + 1;
+		iBounces = 1;
 	}
 	
-	public int Throw (int dices, int bounces)
+	public Dice (int bounces)
+	{
+		iBounces = bounces;
+	}
+	
+	public int Randomize(int bounces)
 	{
 		Random rand = new Random();
+		
 		for (int i = 1; i<= bounces; i++)
 		{
 			iValue = iValue + rand.nextInt(6) + 1;
 		}
-		iValue = iValue / bounces;
-		
-		return iThrow = dices * iValue;
+		return iValue = iValue / bounces;
+	}
+	
+	public int Throw (int dices)
+	{		
+		return iThrow = this.Randomize(dices * iBounces);
+	}
+	
+	public int Throw (int dices, int bounces)
+	{		
+		return iThrow = this.Randomize(dices * bounces);
 	}
 }
